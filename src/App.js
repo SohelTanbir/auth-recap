d26eec027489d44e31fd38bab5a0f3165a43e052
SohelTanbir <soheltanbir19@gmail.com>
2021-03-17 10:22:28 +0600

    auth-recap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app();
+}
+
 
 function App() {
+    const [user, setUser] = useState({})
+      var gProvider = new firebase.auth.GoogleAuthProvider();
+      var gitProvider = new firebase.auth.GithubAuthProvider();
+      var fbProvider = new firebase.auth.FacebookAuthProvider();
+      const googleSignIn = ()=>{
+        firebase.auth()
+      .signInWithPopup(gProvider)
+      .then((result) => {
+        var credential = result.credential;
+        // var token = credential.accessToken;
+        var user = result.user;
+        setUser(user)
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorMessage)
+      });
+  }
+
+  const handleGithubSignIn = ()=>{
+    firebase
+    .auth()
+    .signInWithPopup(gitProvider)
+    .then((result) => {
+      var credential = result.credential;
+  
+      var token = credential.accessToken;
+  
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user)
+    }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      var credential = error.credential;
+      console.log(errorMessage)
+    });
+  }
+// facebook authentication
+
+const handleFacebookSignIn = ()=>{
+  firebase
+  .auth()
+  .signInWithPopup(fbProvider)
+  .then((result) => {
+    var credential = result.credential;
+    var user = result.user;
+    console.log('facebook auth ', result )
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    console.log(errorMessage)
+  });
+
+
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome to User Authentication System</h2>
+        <button onClick={googleSignIn}>SignIn with google</button> <br/><br/>
+        <button onClick={handleGithubSignIn}>SignIn with Github</button> <br/><br/>
+        <button onClick={handleFacebookSignIn}>SignIn with Facebook</button>
+      <p>Email: {user.email}</p>
+      <p>Name: {user.displayName}</p>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+const firebaseConfig = {
+  apiKey: "AIzaSyD7WpyEwAQ3TTmmHjnWkI4hgVL6t5DhXHk",
+  authDomain: "auth-recap-d3ce3.firebaseapp.com",
+  projectId: "auth-recap-d3ce3",
+  storageBucket: "auth-recap-d3ce3.appspot.com",
+  messagingSenderId: "391197058172",
+  appId: "1:391197058172:web:9b4fe35838ee11a41dd5f9"
+};
+
+export default firebaseConfig;
